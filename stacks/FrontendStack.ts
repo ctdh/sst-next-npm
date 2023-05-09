3a789f0233b43346a64763512d7a65358ee3e82b
C H <charles.hewitt@pathwayanalytics.com>
2023-05-10 00:43:18 +0100

    sst update v2.8.14

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -3,14 +3,16 @@ import { StackContext, NextjsSite, StaticSite } from "sst/constructs";
 
 export function FrontendStack({ stack, app }: StackContext) {
   const site = new NextjsSite(stack, "Site", {
+  // This command builds without an error:
+  // const site = new StaticSite(stack, "Site", {
   path: "frontend",
 
-  customDomain: 
-    app.stage === "prod" ? 
-      {
-        domainName: `${app.stage}-next.pathwayanalytics.com`,
-        hostedZone: "pathwayanalytics.com",
-      } : undefined,
+  // customDomain: 
+  //   app.stage === "prod" ? 
+  //     {
+  //       domainName: `${app.stage}-next.mydomain.com`,
+  //       hostedZone: "mydomain.com",
+  //     } : undefined,
   });
   stack.addOutputs({
     siteURL: site.url,
