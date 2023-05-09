5295988d95446e2218a7702fb952c530b8a5301b
C H <charles.hewitt@pathwayanalytics.com>
2023-05-09 19:56:10 +0100

    added customDomain

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -4,6 +4,7 @@ import { StackContext, NextjsSite, StaticSite } from "sst/constructs";
 export function FrontendStack({ stack }: StackContext) {
   const site = new StaticSite(stack, "Site", {
   path: "frontend/",
+  customDomain: undefined,
   });
   stack.addOutputs({
     siteURL: site.url,
