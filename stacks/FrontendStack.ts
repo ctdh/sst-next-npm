0b56a76279b7d24f132b9391785b37473db5fa4a
C H <charles.hewitt@pathwayanalytics.com>
2023-05-09 19:58:34 +0100

    path trailing slash removed

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -3,7 +3,7 @@ import { StackContext, NextjsSite, StaticSite } from "sst/constructs";
 
 export function FrontendStack({ stack }: StackContext) {
   const site = new StaticSite(stack, "Site", {
-  path: "frontend/",
+  path: "frontend",
   customDomain: undefined,
   });
   stack.addOutputs({
